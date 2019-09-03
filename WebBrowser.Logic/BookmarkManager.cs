a1a80c28aa87540457c8a795597b949b060c9da6
Emily Huang <pzh0032@tigermail.auburn.edu>
2019-09-02 21:23:50 -0500

    Added a method to add a bookmark item to the database

--- WebBrowser.Logic/BookmarkManager.cs
+++ WebBrowser.Logic/BookmarkManager.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebBrowser.Data.BookmarksDataSetTableAdapters;
 
 namespace WebBrowser.Logic
 {
     public class BookmarkManager
     {
+        public static void AddBookmarkItem(BookmarkItem item)
+        {
+            var adapter = new BookmarksTableAdapter();
+            adapter.Insert(item.URL, item.Title);
+        }
+
+        public static List<HistoryItem> GetHistoryItems()
+        {
+            var adapter = new HistoryTableAdapter();
+            var results = new List<HistoryItem>();
+            var rows = adapter.GetData();
+
+            foreach (var row in rows)
+            {
+                var item = new HistoryItem();
+                item.URL = row.URL;
+                item.Title = row.Title;
+                item.Date = row.Date;
+
+                results.Add(item);
+            }
+            return results;
+        }
     }
 }
