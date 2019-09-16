502dc22bfbeeb5c3556587cbb3ba3b8cb7cbc75e
Emily Huang <pzh0032@tigermail.auburn.edu>
2019-09-15 20:20:42 -0500

    Added a Delete and a Clear History buttons

--- WebBrowser.UI/HistoryManagerForm.Designer.cs
+++ WebBrowser.UI/HistoryManagerForm.Designer.cs
@@ -33,6 +33,8 @@
             this.searchTerm = new System.Windows.Forms.TextBox();
             this.searchButton = new System.Windows.Forms.Button();
             this.clearButton = new System.Windows.Forms.Button();
+            this.deleteHistoryButton = new System.Windows.Forms.Button();
+            this.clearAllButton = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,21 +44,23 @@
             this.listBoxHistoryManager.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBoxHistoryManager.FormattingEnabled = true;
             this.listBoxHistoryManager.ItemHeight = 27;
-            this.listBoxHistoryManager.Location = new System.Drawing.Point(0, 79);
+            this.listBoxHistoryManager.Location = new System.Drawing.Point(0, 120);
             this.listBoxHistoryManager.Name = "listBoxHistoryManager";
-            this.listBoxHistoryManager.Size = new System.Drawing.Size(1311, 707);
+            this.listBoxHistoryManager.Size = new System.Drawing.Size(1311, 666);
             this.listBoxHistoryManager.TabIndex = 0;
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel1.Controls.Add(this.clearAllButton);
+            this.panel1.Controls.Add(this.deleteHistoryButton);
             this.panel1.Controls.Add(this.clearButton);
             this.panel1.Controls.Add(this.searchButton);
             this.panel1.Controls.Add(this.searchTerm);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1311, 79);
+            this.panel1.Size = new System.Drawing.Size(1311, 120);
             this.panel1.TabIndex = 1;
             // 
             // searchTerm
@@ -89,6 +93,26 @@
             this.clearButton.UseVisualStyleBackColor = true;
             this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
             // 
+            // deleteHistoryButton
+            // 
+            this.deleteHistoryButton.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteHistoryButton.Location = new System.Drawing.Point(20, 71);
+            this.deleteHistoryButton.Name = "deleteHistoryButton";
+            this.deleteHistoryButton.Size = new System.Drawing.Size(138, 38);
+            this.deleteHistoryButton.TabIndex = 3;
+            this.deleteHistoryButton.Text = "Delete";
+            this.deleteHistoryButton.UseVisualStyleBackColor = true;
+            // 
+            // clearAllButton
+            // 
+            this.clearAllButton.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearAllButton.Location = new System.Drawing.Point(176, 71);
+            this.clearAllButton.Name = "clearAllButton";
+            this.clearAllButton.Size = new System.Drawing.Size(197, 38);
+            this.clearAllButton.TabIndex = 4;
+            this.clearAllButton.Text = "Clear History";
+            this.clearAllButton.UseVisualStyleBackColor = true;
+            // 
             // HistoryManagerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
@@ -112,5 +136,7 @@
         private System.Windows.Forms.Button searchButton;
         private System.Windows.Forms.TextBox searchTerm;
         private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button clearAllButton;
+        private System.Windows.Forms.Button deleteHistoryButton;
     }
 }
